c27dabec3b0d65a04595613b96a3d4e79fd00abe
kmolski <starfighter413@gmail.com>
2021-08-17 20:43:56 +0200

    Add time windows with adjustable size

--- sql_generator.py
+++ sql_generator.py
@@ -18,17 +18,14 @@ class Select:
         columns_desc = ", ".join(
             c.emit() if isinstance(c, Computed) else str(c) for c in self.columns
         )
-        return "\n".join(
-            (
-                f"SELECT {columns_desc}",
-                self.from_clause.emit(),
-                self.where_clause.emit(),
-                self.group_by_clause.emit(),
-            )
-        )
+        sql_source = f"SELECT {columns_desc}"
+        for clause in (self.from_clause, self.where_clause, self.group_by_clause):
+            if clause:
+                sql_source += f"\n{clause.emit()}"
+        return sql_source
 
     def emit_print(self):
-        return print(self.emit())
+        print(self.emit(), "\n")
 
     def from_(self, tables):
         self.from_clause = From(tables)
@@ -38,8 +35,14 @@ class Select:
         self.where_clause = Where(condition)
         return self
 
-    def groupByTimeWindow(self, field, time_unit):
-        self.group_by_clause = GroupByTimeWindow(field, time_unit, self)
+    def groupByTimeWindowFixed(self, field, time_unit):
+        self.group_by_clause = GroupByTimeWindowFixed(field, time_unit, self)
+        return self
+
+    def groupByTimeWindowAdjustable(self, field, time_unit, width):
+        self.group_by_clause = GroupByTimeWindowAdjustable(
+            field, time_unit, width, self
+        )
         return self
 
 
@@ -59,13 +62,30 @@ class Where:
         return f"WHERE {self.condition}"
 
 
-class GroupByTimeWindow:
-    TIME_UNITS = ["YEAR", "MONTH", "DAY", "HOUR", "MINUTE", "SECOND"]
+TIME_UNITS = ["YEAR", "MONTH", "DAY", "HOUR", "MINUTE", "SECOND"]
 
+
+class GroupByTimeWindowFixed:
     def __init__(self, column, time_unit, select_clause):
         time_unit = time_unit.upper()
-        time_units_slice = self.TIME_UNITS[: self.TIME_UNITS.index(time_unit) + 1]
+        time_units_slice = TIME_UNITS[: TIME_UNITS.index(time_unit) + 1]
         self.group_exprs = [f"DATEPART({u}, {column})" for u in time_units_slice]
+
+        select_clause.columns.remove(column)
+        select_clause.columns.extend(self.group_exprs)
+
+    def emit(self):
+        group_desc = ", ".join(self.group_exprs)
+        return f"GROUP BY {group_desc}"
+
+
+class GroupByTimeWindowAdjustable:
+    def __init__(self, column, time_unit, width, select_clause):
+        time_unit = time_unit.upper()
+        *time_units_prefix, last_unit = TIME_UNITS[: TIME_UNITS.index(time_unit) + 1]
+        self.group_exprs = [f"DATEPART({u}, {column})" for u in time_units_prefix] + [
+            f"DATEDIFF_BIG({last_unit}, 0, {column}) / {width}"
+        ]
 
         select_clause.columns.remove(column)
         select_clause.columns.extend(self.group_exprs)
@@ -77,4 +97,8 @@ class GroupByTimeWindow:
 
 Select("order_time", Computed("SUM(item_count)", "order_count")).from_("Orders").where(
     "order_time BETWEEN '1970-01-01' AND '2000-01-01'"
-).groupByTimeWindow("order_time", "month").emit_print()
+).groupByTimeWindowFixed("order_time", "month").emit_print()
+
+Select("tstamp", Computed("SUM(item_count)", "ITEM_COUNT")).from_(
+    "Orders"
+).groupByTimeWindowAdjustable("tstamp", "day", 10).emit_print()
