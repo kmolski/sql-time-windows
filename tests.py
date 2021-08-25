8cf0d43c5ddd97bf7b2e2e63c3345f97749fb80a
Konfirm44 <tomsitek@gmail.com>
2021-08-25 21:47:33 +0200

    Added time_window tool

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 from sql_generator import Select, Computed, GroupByTimeWindow
+import time_window
 
 # Select(Computed("SUM(item_count)", "order_count")).from_("Orders").where(
 #     "order_time BETWEEN '1970-01-01' AND '2000-01-01'"
@@ -17,3 +18,14 @@ from sql_generator import Select, Computed, GroupByTimeWindow
 # ).groupByTimeWindowAdjustable("timestamp", "month", 2, 0).emit_print()
 
 # print(GroupByTimeWindow(None, "timestamp", "month").sql_strings())
+
+# time_window.main([])
+time_window.main(["-c", "tstamp"])
+time_window.main(["-c", "tstamp", "-u", "day"])
+time_window.main(["-c", "tstamp", "-u", "day", "-w", "2"])
+time_window.main(["--column", "tstamp", "-u", "day", "--width", "2", "-o", "3"])
+time_window.main(["-c", "tstamp", "-u", "day", "--offset", "3"])
+time_window.main(["--unit", "xdd", "-o", "3"])
+# time_window.main(["-c", "tstamp", "-u", "day", "-w", "2", "-o", "3", "-x"])
+# time_window.main(["-c", "tstamp", "-u", "day", "-w", "2", "-o", "3", "--xd"])
+# time_window.main(["-c", "tstamp", "-u", "day", "-w", "2", "-o", "3", "xd"])
